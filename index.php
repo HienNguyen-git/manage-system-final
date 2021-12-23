ab990fd24cfacd187d52e21c235280b6aff48d10
hiennguyen <nguyentronghien.contact@gmail.com>
2021-12-23 11:14:25 +0700

    Change root to local

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/style.css"> <!-- Sử dụng link tuyệt đối tính từ root, vì vậy có dấu / đầu tiên -->
+	<!-- <link rel="stylesheet" href="/style.css"> Sử dụng link tuyệt đối tính từ root, vì vậy có dấu / đầu tiên -->
+	<link rel="stylesheet" href="style.css"> <!-- Change -->
 	<title>Home Page</title>
 </head>
 
