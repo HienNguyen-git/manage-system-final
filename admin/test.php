068b1cb1dd764f3653b4798524961d4e8036e5c7
haidang2001 <nchdang16012001@gmail.com>
2021-12-29 23:30:04 +0700

    test.php note

--- admin/test.php
+++ admin/test.php
@@ -3,4 +3,12 @@
     // manager_to_employee('binhbinh');
     // print_r(current_manager('it'));
     // update_to_manager('bunnnguyen','it');
+
+    //admin
+    //sửa ngày còn lại, đã sử dụng khi ấn agree,not agree trong absence request ở view detail
+    //sửa department, role trong account chức năng add account
+
+    //manager
+    //làm những chức năng task
+    //sửa ngày còn lại, đã sử dụng khi ấn agree,not agree trong absence request ở view detail
 ?>
