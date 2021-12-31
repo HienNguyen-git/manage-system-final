12d9e03d784f85c7f1380e3406dc3f1abd4ee69b
hiennguyen <nguyentronghien.contact@gmail.com>
2021-12-31 21:24:38 +0700

    Merge

--- manager/test.php
+++ manager/test.php
@@ -8,7 +8,6 @@
     // }
     // update_approval_date('hiengay');
     // print_r(get_taskdetail_byid(1));
-    print_r(get_deparment_byuser('hiengay'));
     // print_r(get_employee_bydepartment('IT'));
     print_r(get_department_byuser('hiengay')['data']['department']);
 ?>
