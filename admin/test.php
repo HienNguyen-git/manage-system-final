9343a7b259582d406746d6ab6b73b01f92de9bfe
haidang2001 <nchdang16012001@gmail.com>
2022-01-02 16:32:46 +0700

    dayoff

--- admin/test.php
+++ admin/test.php
@@ -10,7 +10,10 @@
     // select_absence_info($user)['dayoff_used']
     // update_dayused('nguyenbibi');
     // print_r(select_manager_name('IT'));
-
+    // update_total_dayoff('nguyendai');
+    // print_r(current_user_of_department('IT'));
+    // update_total_dayoff('nguyendai',12);
+    update_to_manager('nguyennhan','IT');
     //admin
     //sửa ngày còn lại, đã sử dụng khi ấn agree,not agree trong absence request ở view detail
     //sửa department, role trong account chức năng add account
